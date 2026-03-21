03b4a5c85c3faf1cbcc0b588177b704b12bbc53f
Jake Morris <jakemorrishello@gmail.com>
2026-03-21 20:58:53 +0100

    Fixed pickup sprites and added value bar

--- item_stats.gd
+++ item_stats.gd
@@ -1,3 +1,4 @@
+@tool
 class_name ItemStats extends RefCounted
 
 enum Item {
@@ -30,21 +31,21 @@ func _init(_name: String, _weight: float, _value: float, _image_path: String = "
 	image_path = _image_path
 
 static var items : Dictionary[Item, ItemStats] = {
-	Item.LA_LAPIN_DE_VICTORIE:    ItemStats.new("La Lapin de Victorie",       30.0,       300000.0, "res://assets/items/la_lapin_de_victorie.png"),
-	Item.ESTAQUE:                 ItemStats.new("Estaque - André DeRain",      2.0,        500000.0, "res://assets/items/estaque.png"),
-	Item.SERPENT_WOOD_CARVING:    ItemStats.new("Serpent from Wood Carving",   2.0,         12000.0, "res://assets/items/serpent_wood_carving.png"),
-	Item.PIERCED_FORM_AMULET:     ItemStats.new("Pierced Form Amulet",        15.0,        111000.0, "res://assets/items/pierced_form_amulet.png"),
-	Item.PURPLE_CAT:              ItemStats.new("Purple Cat - Bill Traylor",   1.0,         10000.0, "res://assets/items/purple_cat.png"),
-	Item.LOBSTER_TELEPHONE:       ItemStats.new("Lobster Telephone",          10.0,        500000.0, "res://assets/items/lobster_telephone.png"),
-	Item.YELLOW_PEONIES:          ItemStats.new("Yellow Peonies - Yaohua",     0.5,         15000.0, "res://assets/items/yellow_peonies.png"),
-	Item.STARRY_NIGHT:            ItemStats.new("Starry Night - Van Gogh",     2.0,     100000000.0, "res://assets/items/starry_night.png"),
-	Item.CLOWN_AU_CHAPEAU_POINTU: ItemStats.new("Clown Au Chapeau Pointu",    2.0,         10000.0, "res://assets/items/clown_au_chapeau_pointu.png"),
-	Item.VASE:                    ItemStats.new("Vase",                       10.0,         20000.0, "res://assets/items/vase.png"),
-	Item.TRANQUIL_NIGHT:          ItemStats.new("Tranquil Night",              2.0,         40000.0, "res://assets/items/tranquil_night.png"),
-	Item.MARILYN_DYPTYCH:         ItemStats.new("Marilyn Dyptych - Andy Warhol", 1.0,     100000.0, "res://assets/items/marilyn_dyptych.png"),
-	Item.CHEETAH_IN_RED:          ItemStats.new("Cheetah in Red - William Skilling", 2.0,  20000.0, "res://assets/items/cheetah_in_red.png"),
-	Item.PLUM_BLOSSOMS_AND_BIRDS: ItemStats.new("Plum Blossoms and Birds",    1.0,         30000.0, "res://assets/items/plum_blossoms_and_birds.png"),
-	Item.WATER_LILIES:            ItemStats.new("Water Lilies - Claude Monet", 1.0,        500000.0, "res://assets/items/water_lilies.png"),
+	Item.LA_LAPIN_DE_VICTORIE:    ItemStats.new("La Lapin de Victorie",            30.0,       300000.0, "res://assets/items/La_Lapin_De_Victoire.png"),
+	Item.ESTAQUE:                 ItemStats.new("Estaque - André DeRain",          2.0,        500000.0, "res://assets/items/Estaque_AndreDerain.png"),
+	Item.SERPENT_WOOD_CARVING:    ItemStats.new("Serpent from Wood Carving",       2.0,         12000.0, "res://assets/items/Serpent_From_Wood_Carving.png"),
+	Item.PIERCED_FORM_AMULET:     ItemStats.new("Pierced Form Amulet",            15.0,        111000.0, "res://assets/items/Pierced_Form_Amulet_BarbaraHepworth.png"),
+	Item.PURPLE_CAT:              ItemStats.new("Purple Cat - Bill Traylor",       1.0,         10000.0, "res://assets/items/Purple_Cat_BillTraylor.png"),
+	Item.LOBSTER_TELEPHONE:       ItemStats.new("Lobster Telephone",              10.0,        500000.0, "res://assets/items/Lobster_Telephone_SalvadorDali.png"),
+	Item.YELLOW_PEONIES:          ItemStats.new("Yellow Peonies - Yaohua",         0.5,         15000.0, "res://assets/items/Yellow_Peonies_YaoHua.png"),
+	Item.STARRY_NIGHT:            ItemStats.new("Starry Night - Van Gogh",         2.0,     100000000.0, "res://assets/items/The_Starry_Night_VincentvanGogh.png"),
+	Item.CLOWN_AU_CHAPEAU_POINTU: ItemStats.new("Clown Au Chapeau Pointu",        2.0,         10000.0, "res://assets/items/Clown_Au_Chapeau_Pointu_BenardBuffet.png"),
+	Item.VASE:                    ItemStats.new("Vase",                           10.0,         20000.0, "res://assets/items/Default_Vase.png"),
+	Item.TRANQUIL_NIGHT:          ItemStats.new("Tranquil Night",                  2.0,         40000.0, "res://assets/items/Tranquil_Night_MontagueDawson.png"),
+	Item.MARILYN_DYPTYCH:         ItemStats.new("Marilyn Dyptych - Andy Warhol",   1.0,        100000.0, "res://assets/items/Marilyn_Diptych_AndyWarhol.png"),
+	Item.CHEETAH_IN_RED:          ItemStats.new("Cheetah in Red - William Skilling", 2.0,       20000.0, "res://assets/items/Cheetah_In_Red_ Harness_WilliamSkilling.png"),
+	Item.PLUM_BLOSSOMS_AND_BIRDS: ItemStats.new("Plum Blossoms and Birds",         1.0,         30000.0, "res://assets/items/Plum_Blossoms_And_Birds_ZhuChan.png"),
+	Item.WATER_LILIES:            ItemStats.new("Water Lilies - Claude Monet",     1.0,        500000.0, "res://assets/items/Water_Lilies_ClaudeMonet.png"),
 }
 
 static func get_item(item: Item) -> ItemStats:
--- pickup.gd
+++ pickup.gd
@@ -1,11 +1,35 @@
+@tool
 class_name Pickup extends Area2D
 
 @onready var sprite: Sprite2D = $Sprite2D
-@export var item: ItemStats.Item
+@export var item: ItemStats.Item:
+	set(value):
+		item = value
+		call_deferred("_update_sprite")
 const HIGHLIGHT_SPEED = 5.
 
+func _ready() -> void:
+	_update_sprite()
+
+func _update_sprite() -> void:
+	var spr := get_node_or_null("Sprite2D") as Sprite2D
+	if not spr:
+		print("Sprite2D not found")
+		return
+	if not ItemStats.items.has(item):
+		print("ItemStats does not have item: %s" % [item])
+		return
+	var image_path := ItemStats.get_item(item).image_path
+	print("image path = %s" % [image_path])
+	if image_path != "":
+		var texture = load(image_path)
+		if texture:
+			spr.texture = texture
+
 func _process(delta: float):
+	if Engine.is_editor_hint():
+		return
 	if Player.closest_pickup == self:
-		sprite.scale = sprite.scale.lerp(Vector2.ONE * 0.35, delta * HIGHLIGHT_SPEED * 2.)
+		sprite.scale = sprite.scale.lerp(Vector2.ONE * 0.85, delta * HIGHLIGHT_SPEED * 2.)
 	else:
-		sprite.scale = sprite.scale.lerp(Vector2.ONE * 0.25, delta * HIGHLIGHT_SPEED)
+		sprite.scale = sprite.scale.lerp(Vector2.ONE * 0.75, delta * HIGHLIGHT_SPEED)
